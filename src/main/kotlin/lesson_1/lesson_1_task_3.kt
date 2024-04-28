c4665d1aac5d3f709e53910a6ff4a5354d565a37
David3415 <dgagiev@gmail.com>
2024-04-28 14:50:18 +0300

    KS-1-3 Task3

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_1
+
+fun main() {
+    val yearOfFlight: Int = 1961
+    var hours: Int = 9
+    var minutes: Int = 7
+
+    println("Данные о полёте Гагарина")
+    println("Дата и время взлёта:")
+    println("год полёта: $yearOfFlight")
+    println("час: $hours")
+    println("минута: $minutes")
+
+    hours = 10
+    minutes = 55
+
+    println("время посадки: $hours : $minutes")
+}
