3c8979298fb840b7049f2419a8257c9aa50fa2d6
David3415 <dgagiev@gmail.com>
2024-04-28 14:21:36 +0300

    KS-1-2 Task2

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_1
+
+fun main() {
+    val orderCount:Int = 75
+    val textThanks:String = "Благодарим за покупку!"
+    println("Количество заказов: $orderCount")
+    println(textThanks)
+    var employersTotalCount:Int = 2000
+    // println("Общее число сотрудников: $employersTotalCount")
+    employersTotalCount--
+    println("Общее число сотрудников: $employersTotalCount")
+}
