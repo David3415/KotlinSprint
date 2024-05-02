2abeaec380a0b435fa975fbdb15c49dfad5e248f
David3415 <dgagiev@gmail.com>
2024-05-02 18:02:27 +0300

    KS-2-3 Task3

---
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_2
+
+fun main() {
+    val departureHour: Int = 9
+    val departureMinute: Int = 39
+    val departureMinutesTotal = departureMinute + departureHour * MINUTES_IN_HOUR
+    val trainTravelTime: Int = 457
+
+    println("Время прибытия поезда ${(departureMinutesTotal + trainTravelTime) / MINUTES_IN_HOUR}:${(departureMinutesTotal + trainTravelTime) % MINUTES_IN_HOUR} ")
+}
+
+const val MINUTES_IN_HOUR: Int = 60
+
