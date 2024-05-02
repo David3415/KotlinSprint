991705d6e116a72a2936cc52bb13b75ef180004d
David3415 <dgagiev@gmail.com>
2024-05-02 15:44:19 +0300

    KS-2-1 Task1

---
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_2
+
+fun main() {
+    val studentPoints1: Int = 3
+    val studentPoints2: Int = 4
+    val studentPoints3: Int = 3
+    val studentPoints4: Int = 5
+    val numOfStudents: Float = 4.0f
+
+    println(String.format("%.2f", (studentPoints1 + studentPoints2 + studentPoints3 + studentPoints4) / numOfStudents))
+
+}
