d8c40942c28ffbf7178a49f9ce6d5c8827414aeb
David3415 <dgagiev@gmail.com>
2024-05-03 12:59:02 +0300

    KS-2-4 Task4 Corrected

--- src/main/kotlin/lesson_2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -1,16 +1,15 @@
 package org.example.lesson_2
 
 fun main() {
-
-    val buffKoef: Float = BUFF / PERCENTS
-    val buffCrystal: String = String.format("%.0f", ORE_KRYSTAL * buffKoef)
-    val buffIron: String = String.format("%.0f", ORE_IRON * buffKoef)
+    val oreCrystal: Int = 7
+    val oreIron: Int = 11
+    val percents: Int = 100
+    val buffKoef: Float = BUFF / percents.toFloat()
+    val buffCrystal: String = String.format("%.0f", oreCrystal * buffKoef)
+    val buffIron: String = String.format("%.0f", oreIron * buffKoef)
     println("Buff кристаллической руды: $buffCrystal")
     println("Buff железной руды: $buffIron")
 
 }
 
-const val ORE_KRYSTAL: Int = 7
-const val ORE_IRON: Int = 11
 const val BUFF: Int = 20
-const val PERCENTS: Float = 100.0f
