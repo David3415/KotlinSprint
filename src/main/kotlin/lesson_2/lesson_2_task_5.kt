74f7fcc9325e8edcab0edd0f34698a8e7f0b26fb
David3415 <dgagiev@gmail.com>
2024-05-03 12:49:59 +0300

    KS-2-5 Task5 Corrected

--- src/main/kotlin/lesson_2/lesson_2_task_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_5.kt
@@ -1,16 +1,17 @@
 package org.example.lesson_2
 
 fun main() {
-    var depositResult: Double = DEPOSIT_AMOUNT.toDouble()
-    for (i in 1..DEPOSIT_PERIOD) {
-        depositResult += depositResult * INTEREST_RATE / PERCENTS
+    val depositAmount: Int = 70000
+    val depositPeriod: Int = 20
+    val percents: Int = 100
+    var depositResult: Double = depositAmount.toDouble()
+    for (i in 1..depositPeriod) {
+        depositResult += depositResult * INTEREST_RATE / percents
     }
 
     val depositResultFinal: String = String.format("%.3f", depositResult)
     println("Размер вклада через 20 лет: $depositResultFinal")
 }
 
-const val DEPOSIT_AMOUNT: Int = 70000
 const val INTEREST_RATE: Double = 16.7
-const val DEPOSIT_PERIOD: Int = 20
-const val PERCENTS: Int = 100
+
