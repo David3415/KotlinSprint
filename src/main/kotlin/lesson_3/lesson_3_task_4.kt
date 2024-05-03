97a4fff61048b5aab61a62c2389f5914620aad50
David3415 <dgagiev@gmail.com>
2024-05-03 14:43:29 +0300

    KS-3-4 Task4

--- src/main/kotlin/lesson_3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -4,11 +4,10 @@ fun main() {
     var moveSource: String = "E2"
     var moveDestination: String = "E4"
     var moveNumber: Int = 1
-    val comment: String = "Номер хода"
-    println("$moveSource - $moveDestination; $comment - $moveNumber")
+    println("$moveSource - $moveDestination; $moveNumber")
 
     moveSource = "D2"
     moveDestination = "D3"
     moveNumber++
-    println("$moveSource - $moveDestination; $comment - $moveNumber")
+    println("$moveSource - $moveDestination; $moveNumber")
 }
