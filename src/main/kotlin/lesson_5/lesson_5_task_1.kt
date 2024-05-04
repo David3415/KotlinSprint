c55787cd462572ab1412836a9a4294194a043b86
David3415 <dgagiev@gmail.com>
2024-05-04 14:10:12 +0300

    KS-5-1 Task1

---
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_5
+
+fun main() {
+
+    val number1: Int = 1
+    val number2: Int = 4
+    var resultSum: Int = number1 + number2
+
+    println("Подтвердите, что вы не бот - решите пример")
+    print("$number1 + $number2 = ")
+
+    val userAnswer: String = readln()
+
+    if (resultSum == userAnswer.toInt()) {
+        println("Добро пожаловать!")
+    } else {
+        println("Доступ запрещен")
+    }
+}
