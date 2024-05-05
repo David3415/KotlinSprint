e47389905d54b46e03dae412c94f7f403393d593
David3415 <dgagiev@gmail.com>
2024-05-05 14:17:09 +0300

    KS-5-2 Task2

--- src/main/kotlin/lesson_5/lesson_5_task_2.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_2.kt
@@ -2,9 +2,7 @@ package org.example.lesson_5
 
 fun main() {
     val currentYear: Int = 2024
-
     println("Введите год рождения:")
-
     val inputYear: Int = readLine()!!.toInt()
     val inputAge: Int = currentYear - inputYear
     if (inputAge >= AGE_OF_MAJORITY) {
