9e4f89a5080893d1db72c264a76d0caeeb28322e
David3415 <dgagiev@gmail.com>
2024-05-05 14:11:01 +0300

    KS-5-1 Task1

--- src/main/kotlin/lesson_5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -1,16 +1,12 @@
 package org.example.lesson_5
 
 fun main() {
-
     val number1: Int = 1
     val number2: Int = 4
-    var resultSum: Int = number1 + number2
-
+    val resultSum: Int = number1 + number2
     println("Подтвердите, что вы не бот - решите пример")
     print("$number1 + $number2 = ")
-
     val userAnswer: String = readln()
-
     if (resultSum == userAnswer.toInt()) {
         println("Добро пожаловать!")
     } else {
