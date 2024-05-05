1a25144e1c2cdf396990e2c07246ebcad8e745a0
David3415 <dgagiev@gmail.com>
2024-05-05 13:28:09 +0300

    KS-4-2 Task1 corrected

--- src/main/kotlin/lesson_4/lesson_4_task_2.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_2.kt
@@ -1,14 +1,18 @@
 package org.example.lesson_4
 
 fun main() {
-
-    var volume: Int = 20
-    var weight: Int = 80
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${weight > MIN_WEIGHT && weight <= MAX_WEIGHT && volume < MAX_VOLUME}")
-
+    var weight: Int = 20
+    var volume: Int = 80
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': " +
+                "${weight > MIN_WEIGHT && weight <= MAX_WEIGHT && volume < MAX_VOLUME}"
+    )
     weight = 50
     volume = 100
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${weight > MIN_WEIGHT && weight <= MAX_WEIGHT && volume < MAX_VOLUME}")
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': " +
+                "${weight > MIN_WEIGHT && weight <= MAX_WEIGHT && volume < MAX_VOLUME}"
+    )
 }
 
 const val MAX_VOLUME: Int = 100
