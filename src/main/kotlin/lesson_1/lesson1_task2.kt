fe72f735cb95e09a6f8bf70c1633b5ea636e0598
David3415 <104300915+David3415@users.noreply.github.com>
2025-12-14 15:35:19 +0300

    KS-1-2 number of workers

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_1
+
+fun main() {
+    val ordersCount: Int = 75
+    val thanksText: String = "Thank you for purchasing!"
+    println("Number of orders: $ordersCount")
+    println("Thanks text: $thanksText")
+    var numberOfWorkers: Int = 2000
+    //println("Number of workers: $numberOfWorkers")
+    numberOfWorkers--
+    println("Number of workers: $numberOfWorkers")
+}
