278cf7f6f55e6632fffa0b9eaf6b2ab36e733d55
David3415 <104300915+David3415@users.noreply.github.com>
2025-12-15 14:33:26 +0300

    KS-2-4 - Kotlin Sprint, Урок 2, Задача 4 руда

---
+++ src/main/kotlin/lesson_2/KS-2-4-ore.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_2
+
+fun main() {
+    val oreKristal: Short = 7
+    val oreMetal: Short = 11
+    val buff:Short = 20
+    val oreKristalBuff: Int = oreKristal*buff/100
+    val oreMetallBuff: Int = oreMetal*buff/100
+    println("totalCrystalBuff: $oreKristalBuff")
+    println("totalMetallBuff: $oreMetallBuff")
+}
