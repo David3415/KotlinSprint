ccf38ef15ee6f90e770c7df982e4f7812703c43a
David3415 <104300915+David3415@users.noreply.github.com>
2025-12-15 15:44:49 +0300

    KS-3-1 - Kotlin Sprint, Урок 3, Задача 1 приветствие на экране

---
+++ src/main/kotlin/lesson_3/KS-3-1-greeting-on-screen.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_3
+
+fun main() {
+    var greeting: String = "Good morning"
+    val name: String = "Tom"
+    println("$greeting, $name!")
+    greeting = "Good evening"
+    println("$greeting, $name!")
+}
