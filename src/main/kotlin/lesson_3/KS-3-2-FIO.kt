0c9db93418c5474628698e128aef6768da17e10b
David3415 <104300915+David3415@users.noreply.github.com>
2025-12-15 16:11:13 +0300

    KS-3-2 - Kotlin Sprint, Урок 3, Задача 2 смена фамилии

---
+++ src/main/kotlin/lesson_3/KS-3-2-FIO.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main() {
+    var family: String = "Andreeva"
+    val name: String = "Tatiana"
+    val surname: String = "Sergeevna"
+    var age: Short = 20
+    println("FIO: $family $name $surname Age: $age")
+    age=22
+    family="Sidorova"
+    println("FIO: $family $name $surname Age: $age")
+}
