6633cfbb3ccf635624f4ff01874de1323b2c53d9
David3415 <104300915+David3415@users.noreply.github.com>
2025-12-16 20:27:22 +0300

    KS-1-5 - Kotlin Sprint, Урок 1, Задача 5 ИСПРАВЛЕННЫЙ number-of-seconds

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,10 +2,14 @@ package org.example.lesson_1
 
 fun main() {
     var seconds: Int = 6480
-    val minutesInHour = 60
-    val secondsInHour = minutesInHour * 60
+    val secondsInHour = SECONDS_IN_MINUTE * MINUTES_IN_HOUR
     val hours: Int = seconds / secondsInHour
-    val minutes: Int = seconds % secondsInHour / minutesInHour
-    seconds = seconds % secondsInHour % minutesInHour
-    println("Time in space: $hours:$minutes:$seconds")
+    val minutes: Int = seconds % secondsInHour / MINUTES_IN_HOUR
+    seconds = seconds % secondsInHour % MINUTES_IN_HOUR
+    val timeResultString = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+    println("Time in space: $timeResultString")
+
 }
+
+const val MINUTES_IN_HOUR = 60
+const val SECONDS_IN_MINUTE = 60
