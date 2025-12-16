bb6ba0ebc18586fd39424c78b251aecc313b4fb7
David3415 <104300915+David3415@users.noreply.github.com>
2025-12-16 20:43:44 +0300

    KS-1-3 - Kotlin Sprint, Урок 1, Задача 3 ИСПРАВЛЕННЫЙ

--- src/main/kotlin/lesson1_task3.kt
+++ src/main/kotlin/lesson1_task3.kt
@@ -1,15 +1,16 @@
 package org.example
 
 fun main() {
-    val ordersCount: Int = 75
-    val thanksText: String = "Thank you for purchasing!"
     val yearOfFlight: Int = 1961
-    var hour: Int = 9
-    var minutes: Int = 7
+    var hour: String = "09"
+    var minutes: String = "07"
+
     println("Year: $yearOfFlight")
     println("Hour of takeOff: $hour")
     println("Minutes of takeOff: $minutes")
-    hour = 10
-    minutes = 55
+
+    hour = "10"
+    minutes = "55"
+
     print("Time of landing: $hour:$minutes")
 }
