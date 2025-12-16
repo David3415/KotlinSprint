90cd96ed5efdf4d42085c8e47ec4dfa345828aa4
David3415 <104300915+David3415@users.noreply.github.com>
2025-12-16 21:07:26 +0300

    KS-2-3 - Kotlin Sprint, Урок 2, Задача 3 train ИСПРАВЛЕННЫЙ

--- src/main/kotlin/lesson_2/KS-2-3-train.kt
+++ src/main/kotlin/lesson_2/KS-2-3-train.kt
@@ -1,19 +1,16 @@
 package org.example.lesson_2
 
 fun main() {
-    val minutesInHour: Short = 60
-    val departureHour: Short = 9
-    val departureMinute: Short = 39
+
     val travelTimeMinutesCommon: Short = 657
-    val travelTimeHours: Int = travelTimeMinutesCommon / minutesInHour
-    val travelTimeMinutes: Int = travelTimeMinutesCommon % minutesInHour
-    var nullForHour: String = ""
-    var nullForMinute: String = ""
-    if (travelTimeHours < 10) {
-        nullForHour = "0"
-    }
-    if (travelTimeMinutes < 10) {
-        nullForMinute = "0"
-    }
-    println("travelTime: $nullForHour$travelTimeHours:$nullForMinute$travelTimeMinutes")
+    val travelTimeHours: Int = travelTimeMinutesCommon / MINUTES_IN_HOUR
+    val travelTimeMinutes: Int = travelTimeMinutesCommon % MINUTES_IN_HOUR
+    val travelTime = String.format("%02d:%02d", travelTimeHours, travelTimeMinutes)
+
+    println("Travel time: $travelTime")
 }
+
+const val MINUTES_IN_HOUR: Short = 60
+
+
+
